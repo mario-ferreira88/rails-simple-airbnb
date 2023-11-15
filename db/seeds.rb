699eb5e1f4a85abdf1e1fe4f684d1ea21195a845
Mario Ferreira <mario.jfs.ferreira@gmail.com>
2023-11-15 16:03:29 +0000

    simple air bnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+light_spacious_garden_flat = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+pizza_east = {
+  name: 'Pizza East',
+  address: 'Pizza Street N10',
+  description: 'Nice place to make pizzas while you rest',
+  price_per_night: 150,
+  number_of_guests: 4
+}
+
+dark_alley_room = {
+  name: 'Dark Alley Room',
+  address: 'Dark Avenue N12',
+  description: 'Very dark, nice sleep in here',
+  price_per_night: 25,
+  number_of_guests: 2
+}
+
+phantasm_flat = {
+  name: 'Phantasm Flat',
+  address: 'Dark Avenue N13',
+  description: 'Can you even see it?',
+  price_per_night: 50,
+  number_of_guests: 3
+}
+
+[light_spacious_garden_flat, pizza_east, dark_alley_room, phantasm_flat].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
